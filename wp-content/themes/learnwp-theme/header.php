276852007aaa0c3e9e7b45c425e273976a866eeb
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-04 07:12:24 +0545

    style sheet linked

--- wp-content/themes/learnwp-theme/header.php
+++ wp-content/themes/learnwp-theme/header.php
@@ -4,5 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>learnWP</title>
+    <link rel="stylesheet" href="<?php get_stylesheet_directory_uri().'./style.css'?>">
 </head>
 <body>
