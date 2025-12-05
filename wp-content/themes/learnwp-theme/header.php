04f6574d2c49cd0283ca8ad29ff81dd02c7f9a51
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-05 07:39:51 +0545

    tailwind setup

--- wp-content/themes/learnwp-theme/header.php
+++ wp-content/themes/learnwp-theme/header.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>learnWP</title>
-    <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri().'./style.css'?>">
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri().'.src/style.css'?>">
+    <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri().'/src/output.css'?>">
 </head>
 <body>
     
--- wp-content/themes/learnwp-theme/index.php
+++ wp-content/themes/learnwp-theme/index.php
@@ -1,5 +1,7 @@
 <?php
 get_header();?>
-<h1> this is header!</h1><?php
+<h1 class="color"> this is header!</h1>
+<button class="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+<?php
 get_footer();
 ?>
