8592eeeeee6e6b4899af0f35da76bc2f3edb4477
Aayush Shrestha <learn.aayush@gmail.com>
2025-12-05 07:49:25 +0545

    functions php added

---
+++ wp-content/themes/learnwp-theme/functions.php
@@ -0,0 +1,3 @@
+<?php 
+wp_enqueue_style('learnwp-style', get_stylesheet(),[],'1.0.0');
+wp_enqueue_style('tailwind',get_template_directory_uri().'/src/output.css',[],'1.0.0');
--- wp-content/themes/learnwp-theme/header.php
+++ wp-content/themes/learnwp-theme/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>learnWP</title>
     <link rel="stylesheet" href="">
-    <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri().'.src/style.css'?>">
-    <link rel="stylesheet" href="<?php  echo get_stylesheet_directory_uri().'/src/output.css'?>">
+    <?php wp_head(); ?>
+   
 </head>
 <body>
     
